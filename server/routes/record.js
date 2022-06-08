949392c848581157538bda1c91e251475ba8a56a
Eren Turan <erenturan41@gmail.com>
2022-06-08 12:13:58 +0300

    server file changes

--- server/routes/record.js
+++ server/routes/record.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 
 const recordRoutes = express.Router();
@@ -5,6 +6,7 @@ const recordRoutes = express.Router();
 const dbo = require("../db/conn");
 const ObjectId = require("mongodb").ObjectId;
 
+// This section will help you get a list of all the records.
 recordRoutes.route("/record").get(function (req,res) {
     let db_connect = dbo.getDb("employees");
     db_connect.collection("records")
@@ -14,3 +16,56 @@ recordRoutes.route("/record").get(function (req,res) {
         res.json(result);
     });
 });
+
+// get a single record by id
+recordRoutes.route("/record/:id").get(function(req, res) {
+    let db_connect = dbo.getDb();
+    let myquery = {_id: ObjectId( req.params.id )};
+    db_connect
+    .collection("records")
+    .findOne(myquery, function (err, result) {
+        if (err) throw err;
+        res.json(result);
+    });
+});
+
+//create new record
+recordRoutes.route("/record/add").post(function(req, response) {
+    let db_connect = dbo.getDb();
+    let myobj = {
+        name: req.body.name,
+        position: req.body.position,
+        level: req.body.level,
+    };
+    db_connect.collection("records").insertOne(myobj, function(err, res) {
+        if (err) throw err;
+        response.json(res);
+    });
+});
+
+//update
+recordRoutes.route("update/:id").post(function(req, response) {
+    let db_connect = dbo.getDb();
+    let myquery = { _id: ObjectId( req.params.id )};
+    let newvalues = {
+        $set: {
+            name: req.body.name,
+            position: req.body.position,
+            level: req.body.level,
+        },
+    }
+});
+
+//delete
+recordRoutes.route("/:id").delete(function(req, res) {
+    let db_connect = dbo.getDb();
+    let myquery = { _id: ObjectId( req.params.id ) };
+    db_connect.collection("records").deleteOne(myquery, function(err, obj) {
+        if (err) throw err;
+        console.log("1 document deleted");
+        res.json(obj);
+    });
+});
+
+module.exports = recordRoutes;
+
