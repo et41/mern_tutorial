09389fea466de5e1d2f306309a2aa079a4a7ee51
Eren Turan <erenturan41@gmail.com>
2022-06-08 14:16:51 +0300

    react files added

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ReactDOMClient from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+const container = document.getElementById("root");
 
-ReactDOM.render(
+const root = ReactDOMClient.createRoot(container);
+
+root.render(
     <React.StrictMode>
         <BrowserRouter>
             <App />  
         </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById("root")
+    </React.StrictMode>
 );
